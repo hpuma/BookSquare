832745298eeb875d61346ca0e523721af817a823
jvberdec <44758976+jvberdec@users.noreply.github.com>
2020-04-12 23:53:21 -0400

    Changed the Main file to begin at the Login page.

--- JavaFx App/application/Main.java
+++ JavaFx App/application/Main.java
@@ -1,27 +1,30 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-
 
 public class Main extends Application {
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			VBox root = /*new BorderPane();*/FXMLLoader.load(getClass().getResource("BookSquare.fxml"));
+//            The login page will be the first that the user sees
+			Parent root = FXMLLoader.load(getClass().getResource("LogIn.fxml"));
 			Scene scene = new Scene(root,1200,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+
+
 			primaryStage.show();
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
