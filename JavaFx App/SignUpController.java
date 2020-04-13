80f00afb2eddc735a949b56c4ece273cd0828307
jvberdec <44758976+jvberdec@users.noreply.github.com>
2020-04-13 00:06:33 -0400

    added controller

---
+++ JavaFx App/SignUpController.java
@@ -0,0 +1,20 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class SignUpController {
+
+    @FXML
+    private AnchorPane signUpWindow;
+
+    @FXML
+//    Once the user signs up for an account, they will be taken to the home page.
+    private void signUp(ActionEvent e) throws IOException {
+        AnchorPane p = FXMLLoader.load(getClass().getResource("BookSquare.fxml"));
+        signUpWindow.getChildren().setAll(p);
+    }
+}
+
