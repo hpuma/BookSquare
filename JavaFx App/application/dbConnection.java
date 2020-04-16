8454382f6161fbfa68dbc967733c44dcfe5419dd
Peter Ye <47184879+PeterYe100@users.noreply.github.com>
2020-04-16 10:59:48 -0400

    Add files via upload

---
+++ JavaFx App/application/dbConnection.java
@@ -0,0 +1,21 @@
+package sample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbConnection {
+    public static Connection connect(){
+        Connection con = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            con = DriverManager.getConnection("jdbc:sqlite:BookSquare.db");
+            System.out.println("Connection established");
+        } catch (ClassNotFoundException | SQLException e){
+            // TODO Auto-generated catch block
+            System.out.println(e+"");
+        }
+        return con;
+    }
+
+}
