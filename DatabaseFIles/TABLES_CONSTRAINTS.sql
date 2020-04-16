6c951a1222c63ac2abf524e3a4546d4698bc5224
hpuma <pumahenry@live.com>
2020-04-15 23:37:55 -0400

    Added_full_working_constraints_clean

--- DatabaseFIles/TABLES_CONSTRAINTS.sql
+++ DatabaseFIles/TABLES_CONSTRAINTS.sql
@@ -1,71 +1,72 @@
 USE csc336;
+
 -- ListingID, ImageID, UserID, TimePosted, Status
-CREATE TABLE IF NOT EXISTS Listings(
-    ListingID INT,
-    ImageID INT,
-    UserID INT,
+CREATE TABLE IF NOT EXISTS Listings (
+    ListingID INT UNSIGNED,
+    ImageID INT UNSIGNED,
+    UserID INT UNSIGNED,
     TimePosted TIMESTAMP,
     Status BIT(1),
-    INDEX(ImageID),
-	PRIMARY KEY (ListingID, UserID, ImageID)
+    PRIMARY KEY (ListingID, UserID, ImageID),
+    INDEX (ImageID)
     );
-    
-CREATE TABLE IF NOT EXISTS ListingImage(
-    ImageID INT,
-    ImgSrc VARCHAR(255),
-    PRIMARY KEY(ImageID,ImgSrc),
-	FOREIGN KEY(ImageID) REFERENCES Listings(ImageID)
-);
--- ListingID, ISBN, Condition, Price .................................
-CREATE TABLE IF NOT EXISTS Product(
-    ListingID INT,
-    ISBN VARCHAR(255),
-    Cond INT, -- Use number codes for its condition
-    Price DECIMAL,
-    INDEX(ISBN),
-    PRIMARY KEY(ListingID),
-	FOREIGN KEY(ListingID) REFERENCES Listings(ListingID)
-);
 
 -- ImageID, Size, Type
+CREATE TABLE IF NOT EXISTS ListingImage (
+    ImageID INT UNSIGNED PRIMARY KEY,
+    ImgSrc VARCHAR(255),
+    FOREIGN KEY (ImageID) REFERENCES Listings(ImageID)
+	ON DELETE CASCADE
+);
 
+-- ListingID, ISBN, Condition, Price .................................
+CREATE TABLE IF NOT EXISTS Product (
+    ListingID INT UNSIGNED,
+    ISBN VARCHAR(255),
+    Cond INT UNSIGNED NOT NULL, -- Use number codes for its condition
+    Price DECIMAL UNSIGNED,
+    PRIMARY KEY (ListingID, ISBN),
+    INDEX (ISBN),
+    FOREIGN KEY (ListingID) REFERENCES Listings (ListingID)
+);
 
 -- TransactionID, BuyerID, SellerID, ListingID, TimeCompleted
-CREATE TABLE IF NOT EXISTS AuditLog(
-    TransID INT,
-    BuyerID INT, 
-    SellerID INT,
-    ListingID INT,
+CREATE TABLE IF NOT EXISTS AuditLog (
+    TransID INT UNSIGNED AUTO_INCREMENT,
+    BuyerID INT UNSIGNED NOT NULL,
+    SellerID INT UNSIGNED NOT NULL,
+    ListingID INT UNSIGNED NOT NULL,
     TimeComplete TIMESTAMP,
-	PRIMARY KEY (TransID, ListingID),
-	FOREIGN KEY (ListingID) REFERENCES Listings(ListingID)
+    PRIMARY KEY (TransID, ListingID),
+    FOREIGN KEY (ListingID) REFERENCES Listings(ListingID)
     );
 
 -- ISBN, Title, Author
-CREATE TABLE IF NOT EXISTS Books(
+CREATE TABLE IF NOT EXISTS Books (
     ISBN VARCHAR(255),
     PRIMARY KEY (ISBN),
-    Title VARCHAR(255),
+    Title VARCHAR(255) NOT NULL,
     Author VARCHAR(255),
-	FOREIGN KEY (ISBN) REFERENCES Product(ISBN) -- ... EHH
+    FOREIGN KEY (ISBN) REFERENCES Product(ISBN) -- ... EHH
 );
 
 -- UserID, FirstName, LastName, Phone, Email, Password
-CREATE TABLE IF NOT EXISTS Users(
-    UserID INT,
+CREATE TABLE IF NOT EXISTS Users (
+    UserID INT UNSIGNED AUTO_INCREMENT,
     FirstName VARCHAR(255),
     LastName VARCHAR(255),
     Phone VARCHAR(255),
     Email VARCHAR(255),
     Pass VARCHAR(255),
-	PRIMARY KEY (UserID, Phone, Email)
+    PRIMARY KEY (UserID, Phone, Email)
     );
+
 
 -- UserID, Avatar, RegDate
 CREATE TABLE IF NOT EXISTS Profiles(
-    UserID INT,
+    UserID INT UNSIGNED,
     Avatar VARCHAR(255), -- ... Path to an image
     RegDate DATE,
-	PRIMARY KEY (UserID),
+    PRIMARY KEY (UserID),
     FOREIGN KEY (UserID) REFERENCES Users(UserID)
     );
