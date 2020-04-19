52c19fceb103410707a51c119aa2ce487e8eaea4
hpuma <pumahenry@live.com>
2020-04-19 19:02:22 -0400

    Finished_Taking_in_valid_input_for_new_listing_need_to_add_db_query_part

--- CSC336-Project/JavaFx App/controllers/newListingController.java
+++ CSC336-Project/JavaFx App/controllers/newListingController.java
@@ -12,6 +12,7 @@ import javafx.stage.FileChooser;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 public class newListingController {
 
@@ -46,7 +47,7 @@ public class newListingController {
     private RadioButton goodButton;
 
     @FXML
-    private RadioButton AcceptableButton;
+    private RadioButton acceptableButton;
 
     @FXML
     private TextField priceTextField;
@@ -64,25 +65,77 @@ public class newListingController {
     void searchButtonClicked(ActionEvent e) throws IOException {
         System.out.println("Books have been searched in the new listings page.");
     }
-
     @FXML
     void logOut(ActionEvent e) throws IOException {
         AnchorPane p = FXMLLoader.load(getClass().getResource("/pages/LogIn.fxml"));
         newListing.getChildren().setAll(p);
     }
-
     @FXML
     void getDashboard(ActionEvent e) throws IOException {
         AnchorPane p = FXMLLoader.load(getClass().getResource("/pages/myDashboard.fxml"));
         newListing.getChildren().setAll(p);
     }
-    @FXML
-    void setCreateListingButtonClicked(ActionEvent e)throws IOException{
-        String title = titleTextField.getText();
-        String author = authorTextField.getText();
-        String ISBN = ISBNTextField.getText();
+// Checks to see if there is a single radio button selected then returns the condition value
+// If somehow, none or all are selected then we return an empty string.
+    private String getSingleButton(RadioButton[] buttonList){
+        int buttonActive = 0;
+        String message = "";
+        for (RadioButton currButton: buttonList){
+            if(currButton.isSelected()){
+                buttonActive +=1;
+                if (buttonActive == 1){
+                    message = currButton.getText();
+                }
+            }
+        }
+        return buttonActive == 1?message:"" ;
     }
-    
+
+    // Updating the radio buttons when pressed.
+    @FXML
+    void setNewButton(ActionEvent e){
+        acceptableButton.setSelected(false);
+        goodButton.setSelected(false);
+        newButton.setSelected(true);
+    }
+    @FXML
+    void setGoodButton(ActionEvent e){
+        newButton.setSelected(false);
+        acceptableButton.setSelected(false);
+        goodButton.setSelected(true);
+    }
+    @FXML
+    void setAcceptableButton(ActionEvent e){
+        newButton.setSelected(false);
+        goodButton.setSelected(false);
+        acceptableButton.setSelected(true);
+    }
+    // Handle when create listing is pressed, I have not queried the database yet or added a list.
+    @FXML
+    void setCreateListingButton(ActionEvent e)throws IOException{
+        try{
+            String title = titleTextField.getText();
+            String author = authorTextField.getText();
+            String ISBN = ISBNTextField.getText();
+            String Condition = getSingleButton(new RadioButton[]{goodButton, newButton, acceptableButton});
+            Double Price = Double.valueOf(priceTextField.getText()); //Will give you a nasty error if not handled!
+
+            // When all the data is VALID, Condition is valid and text is not empty!
+            if (title.isEmpty() ^ author.isEmpty() ^ ISBN.isEmpty() ^ Price.isNaN() ^ !Condition.isEmpty()){
+                System.out.println(title+"\n"+author+"\n"+ISBN+"\n"+Condition);
+                System.out.println(Price);
+            }else{
+                System.out.println("INVALID DATA ");
+            }
+        }
+        catch (NullPointerException ex){
+            System.out.println("UNACCEPTABLE PRICE");
+            ex.printStackTrace();
+        }
+        catch (NumberFormatException ex){
+            System.out.println("PRICE IS NOT VALID");
+        }
+    }
     @FXML
     void uploadListingImage(ActionEvent event) {
     	System.out.println("boo");
