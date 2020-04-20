2753b82c5b786b0ff342aff21ba1419b3add818a
hpuma <pumahenry@live.com>
2020-04-20 02:49:07 -0400

    Added_full_listing_functionality_and_new_listing_logo_works_KINDA

--- CSC336-Project/JavaFx App/controllers/newListingController.java
+++ CSC336-Project/JavaFx App/controllers/newListingController.java
@@ -10,20 +10,21 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
-
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.sql.*;
 import java.util.Date;
 
 public class newListingController {
     // Path to the current image to upload to the database.
-    public static String imageName = "file:"+System.getProperty("user.dir") + "\\images\\gettyimages-766410071-612x612.jpg";
+    public static String imageName = "file:" + System.getProperty("user.dir") + "\\images\\gettyimages-766410071-612x612.jpg";
+    @FXML
+    private ImageView storeLogo;
+
     @FXML
     private AnchorPane newListing;
 
@@ -70,70 +71,83 @@ public class newListingController {
     private Button uploadButton;
 
     @FXML
+    public void logoClicked(MouseEvent mouseEvent) throws IOException {
+        AnchorPane p = FXMLLoader.load(getClass().getResource("/pages/BookSquare.fxml"));
+        newListing.getChildren().setAll(p);
+        System.out.println("LOGOCLICKED");
+    }
+
+    @FXML
     void searchButtonClicked(ActionEvent e) throws IOException {
         System.out.println("Books have been searched in the new listings page.");
     }
+
     @FXML
     void logOut(ActionEvent e) throws IOException {
         AnchorPane p = FXMLLoader.load(getClass().getResource("/pages/LogIn.fxml"));
         newListing.getChildren().setAll(p);
     }
+
     @FXML
     void getDashboard(ActionEvent e) throws IOException {
         AnchorPane p = FXMLLoader.load(getClass().getResource("/pages/myDashboard.fxml"));
         newListing.getChildren().setAll(p);
     }
-// Checks to see if there is a single radio button selected then returns the condition value
+
+    // Checks to see if there is a single radio button selected then returns the condition value
 // If somehow, none or all are selected then we return an empty string meaning that there is a non-valid condition.
-    private int getSingleButton(RadioButton[] buttonList){
+    private int getSingleButton(RadioButton[] buttonList) {
         int buttonActive = 0;
         String message = "";
-        for (RadioButton currButton: buttonList){
-            if(currButton.isSelected()){
+        for (RadioButton currButton : buttonList) {
+            if (currButton.isSelected()) {
                 return buttonActive;
             }
-            buttonActive+=1;
+            buttonActive += 1;
         }
         return -1; // Rare case where none of them are active
     }
+
     // Updating the radio buttons when pressed so only one button is selected at a time.
     @FXML
-    void setNewButton(ActionEvent e){
+    void setNewButton(ActionEvent e) {
         acceptableButton.setSelected(false);
         goodButton.setSelected(false);
         newButton.setSelected(true);
     }
+
     @FXML
-    void setGoodButton(ActionEvent e){
+    void setGoodButton(ActionEvent e) {
         newButton.setSelected(false);
         acceptableButton.setSelected(false);
         goodButton.setSelected(true);
     }
+
     @FXML
-    void setAcceptableButton(ActionEvent e){
+    void setAcceptableButton(ActionEvent e) {
         newButton.setSelected(false);
         goodButton.setSelected(false);
         acceptableButton.setSelected(true);
     }
+
     // Handle when create listing is pressed, I have not queried the database yet or added a list.
     @FXML
-    void setCreateListingButton(ActionEvent e){
-        try{
+    void setCreateListingButton(ActionEvent e) {
+        try {
             // Retrieving all the input data.
             String title = titleTextField.getText();
             String author = authorTextField.getText();
             String isbn = ISBNTextField.getText();
             int condition = getSingleButton(new RadioButton[]{acceptableButton, goodButton, newButton});
-            double Price = Double.parseDouble(priceTextField.getText()); //Will give you a nasty error if not handled!
+            double price = Double.parseDouble(priceTextField.getText()); //Will give you a nasty error if not handled!
 
+            // When all the data is VALID, Condition is valid and text is not empty! We can do better.
+            if (title.isEmpty() ^ author.isEmpty() ^ isbn.isEmpty() ^ Double.isNaN(price) ^ (condition >= 0)) {
 
-            // When all the data is VALID, Condition is valid and text is not empty!
-            if (title.isEmpty() ^ author.isEmpty() ^ isbn.isEmpty() ^ Double.isNaN(Price) ^ (condition >= 0)){
-
-                // Getting UserID
+                // Getting UserID.
                 int currentUserID = loginController.currAccount.getUserID();
 
-                // Create the TimeStamp Column
+                // Create the TimeStamp Column.
                 Date now = new Date();
                 Timestamp currentTime = new Timestamp(now.getTime());
 
@@ -145,47 +159,50 @@ public class newListingController {
                         "ON Listings.ListingID = Product.ListingID)\n" +
                         "WHERE Listings.UserID =%d " +
                         "AND Product.ISBN ='%s' " +
-                        "AND Product.Cond='%s'" + "AND Listings.Status=1;",currentUserID,isbn,condition);
+                        "AND Product.Cond='%s'" + "AND Listings.Status=1;", currentUserID, isbn, condition);
 
                 // Executing the query to find a duplicate.
-                try(ResultSet listingSearch =  listingQuery.executeStatement(findListingQuery)){
-                    if(listingSearch.next()){
+                try (ResultSet listingSearch = listingQuery.executeStatement(findListingQuery)) {
+                    if (listingSearch.next()) {
                         System.out.println("FOUND BOOK");
-                        System.out.println(listingSearch.getString("ListingID") +"\t" +
-                                listingSearch.getInt("UserID") +"\t"+
-                                listingSearch.getString("ISBN")+"\t"+
+                        System.out.println(listingSearch.getString("ListingID") + "\t" +
+                                listingSearch.getInt("UserID") + "\t" +
+                                listingSearch.getString("ISBN") + "\t" +
                                 listingSearch.getBigDecimal("Price"));
                     }
                     // ADD THE ITEM HERE, SET ADD QUERIES HERE.
-                    System.out.println("BOOK NOT FOUND");
+                    System.out.println("THIS BOOK IS UNIQUE");
 
-                    // Creating the ListingID
-                    String hashMaker = isbn+currentUserID+Price;
+                    // Creating the ListingID. Hashcode creates a large number for listingID :/
+                    String hashMaker = isbn + currentUserID + price;
                     int newListingID = Math.abs(hashMaker.hashCode());
 
-                    // Inserting to listings
-                    InsertTableData.insertListings(newListingID,currentUserID,currentTime,true);
-                    System.out.println(imageName);
-                    InsertTableData.insertImage(newListingID,imageName);
+                    // Inserting to all the tables.
+                    InsertTableData.insertListings(newListingID, currentUserID, currentTime, true);
+                    InsertTableData.insertImage(newListingID, imageName);
+                    InsertTableData.insertBooks(isbn, title, author);
+                    InsertTableData.insertProduct(isbn, newListingID, condition, price);
 
-
-
-                } catch (SQLException ex){ System.out.println("INVALID DATA 1");}
-            } else{ System.out.println("INVALID DATA 2 "); }
-        } catch (NullPointerException ex){
+                } catch (SQLException ex) {
+                    System.out.println("INVALID DATA 1: QUERY CHECK FAILED");
+                }
+            } else {
+                System.out.println("INVALID DATA 2: INVALID DATA SOMEWHERE ");
+            }
+        } catch (NullPointerException ex) {
             System.out.println("UNACCEPTABLE PRICE");
-        }
-        catch (NumberFormatException ex){
+        } catch (NumberFormatException ex) {
             System.out.println("PRICE IS NOT VALID");
         }
     }
+
     @FXML
     void uploadListingImage(ActionEvent event) throws MalformedURLException {
-    	//System.out.println("boo");
-    	FileChooser fileChooser = new FileChooser();//opens file explorer
-    	File selectedFile = fileChooser.showOpenDialog(newListing.getScene().getWindow());
-    	String imagePath = selectedFile.getAbsolutePath();//get file path
-    	System.out.println(imagePath);
+        //System.out.println("boo");
+        FileChooser fileChooser = new FileChooser();//opens file explorer
+        File selectedFile = fileChooser.showOpenDialog(newListing.getScene().getWindow());
+        String imagePath = selectedFile.getAbsolutePath();//get file path
+        System.out.println(imagePath);
         imageName = selectedFile.toURI().toString();
         Image image = new Image(imageName);//change image to selected image
         uploadImage.setImage(image);
