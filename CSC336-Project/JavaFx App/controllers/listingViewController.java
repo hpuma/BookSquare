c2d455b15d3f6386e29e21630e52aa7e1e605dc5
jvberdec <44758976+jvberdec@users.noreply.github.com>
2020-04-20 11:00:07 -0400

    added variables and methods
    to change values in book pop up

--- CSC336-Project/JavaFx App/controllers/listingViewController.java
+++ CSC336-Project/JavaFx App/controllers/listingViewController.java
@@ -1,4 +1,40 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
 public class listingViewController {
+
+    @FXML
+    private Label bookTitle;
+
+    @FXML
+    private Label bookAuthor;
+
+    @FXML
+    private Label bookISBN;
+
+    @FXML
+    private Label sellerName;
+
+    @FXML
+    private Label sellerID;
+
+    @FXML
+    private Label sellerEmail;
+
+    @FXML
+    private Label sellerPhone;
+
+
+    public void setSellerLabels(String title, String author, String ISBN, String userFullName, String userID, String email, String phone){
+        bookTitle.setText(title);
+        bookAuthor.setText(author);
+        bookISBN.setText(ISBN);
+        sellerName.setText(userFullName);
+        sellerID.setText(userID);
+        sellerEmail.setText(email);
+        sellerPhone.setText(phone);
+    }
 }
