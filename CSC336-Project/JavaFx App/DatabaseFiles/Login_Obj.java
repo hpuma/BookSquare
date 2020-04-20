984606bc97afe79ecee98827a3092ce4714a012d
hpuma <pumahenry@live.com>
2020-04-20 14:03:16 -0400

    Fixed_login_issue

--- CSC336-Project/JavaFx App/DatabaseFiles/Login_Obj.java
+++ CSC336-Project/JavaFx App/DatabaseFiles/Login_Obj.java
@@ -9,21 +9,26 @@ import java.sql.SQLException;
 //Declare this object when checking for valid users.
 //How to declare this object: Login_Obj login = new Login_Obj();
 public class Login_Obj {
+    Connection c;
     // Revised the verify login, it connects to the database and queries the user table.
-    public boolean valid_login(String email, String password) throws Exception{
+    public boolean valid_login(String email, String password) {
         // Safe way of formatting the SQL query
-        if(email.isEmpty() || password.isEmpty()){
+        if (email.isEmpty() || password.isEmpty()) {
             return false;
         }
-
-        String check = String.format("SELECT * FROM Users WHERE Email=\'%s\'"+" AND Pass=\'%s\';",email,password);
-        executeScript runQuery = new executeScript();
-        try(ResultSet r = runQuery.executeStatement("users",check)){
-            return r != null;
+        try {
+            c = dbConnection.connect();
+            PreparedStatement ps = c.prepareStatement("SELECT * FROM Users WHERE Email=? AND Pass=?;");
+            ps.setString(1, email);
+            ps.setString(2, password);
+            ResultSet r = ps.executeQuery();
+            if (r != null) { // The current input has been successfully queried in the database.
+                return r.getString("Email").equals(email) && r.getString("Pass").equals(password); // Double check for verification
+            } else {
+                return false; // The Current User does not exist.
+            }
+        } catch (NullPointerException | SQLException n) {
+            return false;
         }
-        catch (NullPointerException n){
-            n.printStackTrace();
-        }
-        return false;
     }
 }
